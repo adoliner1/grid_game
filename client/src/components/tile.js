ad24b3954c9433fd04c390ed35dfca71edb45595
adoliner1 <adoliner@gmail.com>
2024-08-27 11:41:51 -0400

    minor balance changes. Wording changes. Formatting changes.

--- client/src/components/tile.js
+++ client/src/components/tile.js
@@ -23,22 +23,20 @@ const Tile = ({
 
     const tileClickHandler = isSelectable() ? onTileClick : undefined
 
-    const formattedDescription = description
-    .replace(/\n/g, '<br><br>')
-    .replace(/Ruler:/g, '<strong>Ruler:</strong>')
-    .replace(/Action:/g, '<strong>Action:</strong>')
-    .replace(/Reaction:/g, '<strong>Reaction:</strong>')
-    .replace(/Most Shapes:/g, '<strong>Most Shapes:</strong>')
-    .replace(/Fewest Shapes:/g, '<strong>Fewest Shapes:</strong>')
-    .replace(/Most Power:/g, '<strong>Most Power:</strong>')
-    .replace(/(\d+)\s+Power/g, '<strong>$1 Power</strong>')
-    .replace(/start of a round/gi, '<i>start of a round</i>')
-    .replace(/end of a round/gi, '<i>end of a round</i>')
-    .replace(/end of the game/gi, '<i>end of the game</i>')
-    .replace(/\b(burn|burns|burned)\b/gi, '<span style="color: #ff8700;">$1</span>')
-    .replace(/\b(receive|receives|received)\b/gi, '<span style="color: #9f00ff;">$1</span>')
-    .replace(/\b(place|places|placed|placing)\b/gi, '<span style="color: #007a9a;">$1</span>')
-    .replace(/\b(produce|produces|produced|producing)\b/gi, '<span style="color: #019000;">$1</span>');
+    function parseCustomMarkup(text) {
+        return text
+          .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')  // Bold
+          .replace(/__(.*?)__/g, '<i>$1</i>')  // Italic
+          .replace(/\^\^(.*?)\^\^/g, '<span style="color: #ff8700;">$1</span>')  // Burn (orange)
+          .replace(/\[\[(.*?)\]\]/g, '<span style="color: #9f00ff;">$1</span>')  // Receive (purple)
+          .replace(/\(\((.*?)\)\)/g, '<span style="color: #007a9a;">$1</span>')  // Place (blue)
+          .replace(/\+\+(.*?)\+\+/g, '<span style="color: #019000;">$1</span>')  // Produce (green)
+          .replace(/\b(action|reaction)\b/gi, '<u>$1</u>')  // Underline action and reaction
+          .replace(/\n/g, '<br><br>');  // Double line breaks
+      }
+      
+
+    description = parseCustomMarkup(description)
 
     return (
         <div className={`${isSelectable() ? 'tile selectable-tile' : 'tile'} ${ruler ? `tile-${ruler}` : ''} ${is_on_cooldown ? 'tile-on-cooldown' : ''}`} onClick={tileClickHandler}>
@@ -46,7 +44,7 @@ const Tile = ({
                 <h3 className={`tile-name ${ruler ? `tile-name-${ruler}` : ''}`}>{name}</h3>
                 <span className="tile-type">{type}</span>
             </div>
-            <p className="tile-description" dangerouslySetInnerHTML={{ __html: formattedDescription }}></p>
+            <p className="tile-description" dangerouslySetInnerHTML={{ __html: description }}></p>
             <p className='red-power'> Red Power: {red_power} </p>
             <p className='blue-power'> Blue Power: {blue_power} </p>
             <ShapesOnTile 
