8f02fb3dadec0ca741c66142983e34c9807e1810
adoliner1 <adoliner@gmail.com>
2024-10-05 11:40:40 -0400

    with database changes

--- fast_api_server/database.py
+++ fast_api_server/database.py
@@ -2,15 +2,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://myuser:mypassword@localhost/mydatabase"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://myuser:mypassword@localhost/mydatabase")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
-# Dependency to get the database session
 def get_db():
     db = SessionLocal()
     try:
