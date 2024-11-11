b80a8dbd7dcdebaa40c4d8c576108933987cfff2
adoliner1 <adoliner@gmail.com>
2024-11-11 09:08:08 -0500

    updated leader board styling

--- client/src/components/lobby.js
+++ client/src/components/lobby.js
@@ -132,94 +132,40 @@ function Lobby() {
   };
 
   return (
-    <div className='lobby-container'>
-      <div className='top-section'>
-        <div className='left-column'>
-          <p>
-            <a href="https://docs.google.com/document/d/1hKATNg-UF-BnJWOyNphiv7hFPWJ81dy6zJYiH3Jl96I/edit?tab=t.0" target="_blank" rel="noopener noreferrer">Rules</a>
-          </p>
-          {user ? (
-            <>
-              {playerInfo && <p>{playerInfo.player_name}</p>}
-              <button onClick={handleLogout}>Logout</button>
-            </>
-          ) : (
-            <button onClick={handleLogin}>Login with Google</button>
-          )}
-        </div>
-        <div className='lobby-tables'>
-          <table>
-            <thead>
-              <tr>
-                <th>Table</th>
-                <th>Players</th>
-              </tr>
-            </thead>
-            <tbody>
-              {lobbyTables.map((lobbyTable) => (
-                <tr
-                  key={lobbyTable.id}
-                  onClick={() => handleJoinLobbyTable(lobbyTable.id)}
-                  className="clickable-row"
-                >
-                  <td>{lobbyTable.name}</td>
-                  <td>
-                    {lobbyTable.players.map(player => 
-                      <span key={player.id} className={player.is_guest ? 'text-gray-500' : 'text-black'}>
-                        {player.name}
-                      </span>
-                    ).reduce((prev, curr) => [prev, ', ', curr])}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className='buttons-column'>
-          <button className='create-lobby-button' onClick={handleCreateLobbyTable}>Create Table</button>
-        </div>
-        <div className='leaderboard'>
-          <p className='leader-board-header'>Leaderboard</p>
-          <table className="w-full">
-            <thead>
-              <tr>
-                <th className="text-left px-2">Rank</th>
-                <th className="text-left px-2">Player</th>
-                <th className="text-right px-2">ELO</th>
-                <th className="text-right px-2">W/L</th>
-              </tr>
-            </thead>
-            <tbody>
-              {leaderboard.map((player, index) => (
-                <tr key={player.username} className="hover:bg-gray-50">
-                  <td className="px-2">{index + 1}</td>
-                  <td className="px-2">
-                    <div className="flex flex-col">
-                      <span>{player.username}</span>
-                      <span className={`text-xs ${getRankClass(player.elo_rating)}`}>
-                        {getRankName(player.elo_rating)}
-                      </span>
-                    </div>
-                  </td>
-                  <td className="text-right px-2">{player.elo_rating}</td>
-                  <td className="text-right px-2 text-sm">
-                    <span className="text-green-600">{player.wins}</span>
-                    {' / '}
-                    <span className="text-red-600">{player.losses}</span>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-      <ChatRoom
-        players={lobbyPlayers}
-        messages={messages}
-        onSendMessage={handleSendMessage}
-        currentPlayer={playerInfo}
-      />
-    </div>
+    <div className='leaderboard'>
+    <h2 className='leader-board-header'>Leaderboard</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Player</th>
+          <th>ELO</th>
+          <th>W/L</th>
+        </tr>
+      </thead>
+      <tbody>
+        {leaderboard.map((player, index) => (
+          <tr key={player.username}>
+            <td>{index + 1}</td>
+            <td>
+              <div className="player-info">
+                <span className="player-name">{player.username}</span>
+                <span className={`player-rank ${getRankClass(player.elo_rating)}`}>
+                  {getRankName(player.elo_rating)}
+                </span>
+              </div>
+            </td>
+            <td>{player.elo_rating}</td>
+            <td>
+              <span className="wins">{player.wins}</span>
+              /
+              <span className="losses">{player.losses}</span>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
   );
 }
 
